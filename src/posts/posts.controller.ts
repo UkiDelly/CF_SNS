e126fa551291fa3c73fcdffd6cfd4800d7a56200
UkiDelly <daehyeon1999@naver.com>
2023-12-27 13:45:32 +0900

    ✨ feat: Get 요청 구현

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
   id: number;
   author: string;
   content: string;
@@ -9,18 +9,44 @@ interface Post {
   commentCount: number;
 }
 
+const posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'John Doe',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    likeCount: 0,
+    commentCount: 0,
+  },
+  {
+    id: 2,
+    author: 'Jane Smith',
+    content: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.',
+    likeCount: 5,
+    commentCount: 2,
+  },
+  {
+    id: 3,
+    author: 'Alice Johnson',
+    content: 'At vero eos et accusamus et iusto odio dignissimos ducimus.',
+    likeCount: 10,
+    commentCount: 3,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  getPosts(): Post {
-    return {
-      id: 1,
-      author: 'John Doe',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      likeCount: 0,
-      commentCount: 0,
-    };
+  getPosts(): PostModel[] {
+    return posts;
   }
+
+  @Get(':id')
+  getPost(@Param('id') id: string): PostModel {
+    return posts.find((post) => post.id === +id);
+  }
+
+  @Post()
+  createPost(): PostModel | void {}
 }
