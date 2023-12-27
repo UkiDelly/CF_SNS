aec2ab0db084357c30fc0c4be312f3a856387e65
UkiDelly <daehyeon1999@naver.com>
2023-12-27 09:37:03 +0900

    🎉  start

---
+++ src/app.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+interface Post {
+  id: number;
+  author: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('posts')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): Post {
+    return {
+      id: 1,
+      author: 'John Doe',
+      content: 'Hello World!',
+      likeCount: 10,
+      commentCount: 10
+    };
+  }
+}
