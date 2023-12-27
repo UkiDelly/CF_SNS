013cce625ee4995aec2ba270f297c375f27de116
Delly <daehyeon1999@naver.com>
2023-12-27 11:49:53 +0900

    🚚 appcontroller의 post을 postcontroller으로 이동

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-interface Post {
-  id: number;
-  author: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
 @Controller('posts')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): Post {
-    return {
-      id: 1,
-      author: 'John Doe',
-      content: 'Hello World!',
-      likeCount: 10,
-      commentCount: 10
-    };
+  getHello(): string {
+    return 'Hello World!';
   }
 }
 
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
+
+interface Post {
+  id: number;
+  author: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  getPosts(): Post {
+    return {
+      id: 1,
+      author: 'John Doe',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      likeCount: 0,
+      commentCount: 0,
+    };
+  }
 }
